1f1f546af889e4c2547dc7dd60063449bc3fea3d
Jing Yang <ditsing@gmail.com>
2016-01-05 16:31:51 +0800

    Named venue and stock in test_exchange.rb.

--- examples/test_exchange.rb
+++ examples/test_exchange.rb
@@ -5,13 +5,17 @@ include StockFighter::ApiMixin
 # Set your API key.
 set_api_key "your_api_key_here"
 
+venue = "TESTEX"
+stock = "FOOBAR"
+account = "EXB123456"
+
 # Test if the service is up.
 ok = send_heartbeat.parsed_response["ok"] rescue false
 
 raise "Can't sent heart beat" unless ok
 
 # Get the current orderbook.
-orderbook = get_orderbook "TESTEX", "FOOBAR"
+orderbook = get_orderbook venue, stock
 
 raise "Can't get orderbook" unless orderbook["ok"]
 
@@ -20,7 +24,7 @@ p "asks: ", orderbook["asks"]
 
 # Place an order!
 # By default the direction is 'buy' and order type is 'limit'.
-order = place_order "TESTEX", "FOOBAR", "account", price: 100, qty: 1, direction: :buy, order_type: :limit
+order = place_order venue, stock, account, price: 100, qty: 1, direction: :buy, order_type: :limit
 
 # There should be an error unless a valid API key is supplied.
 p order
